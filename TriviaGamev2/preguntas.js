1188d4a07dff38521921df38fd5e811d628e4e5d
Rodrigo Reyes <98792953+rodrigo-reyes001@users.noreply.github.com>
2022-02-15 18:39:22 -0600

    Add files via upload

---
+++ TriviaGamev2/preguntas.js
@@ -0,0 +1,243 @@
+var uno = {
+    "pregunta1": "¿Cual es el nombre real de Snoop Dogg?",
+    "respuestaC1": "Calvin Cordozar Broadus Jr.",
+    "respuesta2": " Jayceon Terrell Taylor",
+    "respuesta3": " Cameron Jibril Thomaz",
+    "respuesta4": " Abel Makkonen Tesfaye"
+}
+
+var dos = {
+        "pregunta2": "¿Cual es el album mas nuevo de Tyler, The Creator (2022)?",
+        "respuesta5": "Igor",
+        "respuesta6": "Goblin",
+        "respuesta7": "Flower Boy",
+        "respuestaC8": "Call me if you get lost"
+    }
+var tres = {
+    "pregunta3": "¿Quien compuso el soundtrack original de Star Wars?",
+        "repuesta9" : "Howard Shore",
+        "respuestaC10": "John Williams",
+        "respuesta11": "Kensuke Ushio",
+        "respuesta12": "Hans Zimmer"
+}
+var fo ={
+    "pregunta4": "Donal Glover Conocido por su nombre artistico: ",
+        "respuesta13": "The Game",
+        "respuesta14": "Lil Nas X",
+        "respuestaC15": "Childish Gambino",
+        "respuesta16": "Kid Cudi"
+}
+var cinco ={
+    "pregunta5": "¿Cual es la cancion mas popular Segun Spotify de Pink Floyd?",
+    "respuesta17": "Another Brick Brick In The Wall, Pt2",
+    "respuestaC18": "Wish You Were Here",
+    "respuesta19": "Money",
+    "respuesta20": "Time"
+}
+var six = {
+    "pregunta6": "¿Cual de estas canciones es de My Chemical Romance?",
+    "respuestaC21": "Helena",
+    "respuesta22": "Death Of A Bachelor",
+    "respuesta23": "Magic",
+    "respuesta24": "Soundtrack 2 My Life"
+}
+var siete = {
+    "pregunta7":"¿Quienes son los integrantes actuales de Paramore?",
+    "respuestaC25": "Hayley Williams, Zac Farro, Taylor York",
+    "respuesta26": "Gerard Way, Mikey Way, Frank Lero, Ray Toro",
+    "respuesta27": "Brendon Urie, Jon Walker, Spencer Smith",
+    "respuesta28": "Ryan Guldemond, Molly Guldemond, Jasmin Parkin"
+}
+var ocho ={
+    "pregunta8": "¿Cual de estos cantantes es el mas joven?",
+    "respuesta29": "Gerard Way",
+    "respuestaC30": "Hayley Williams",
+    "respuesta31": "Avril Lavigne",
+    "respuesta32": "Ronnie Radke"
+}
+var nueve = {
+    "pregunta9": "¿Quien Compuso la cancion de tema de 'The Mandalorian'",
+    "respuesta33": "John Williams",
+    "respuesta34": "John Adams",
+    "respuestaC35": "Ludwig Göransson",
+    "respuesta36": "Philip Glass"
+}
+var diez = {
+    "pregunta10" :"Se rindió homenaje a la ex mujer de David Bowie con una canción de The Rolling Stones, ¿nombre de la canción?",
+    "respuesta37": "Wild Horses",
+    "respuesta38": "Gimme Shelter",
+    "respuestaC39": "Angie",
+    "respuesta40": "Satisfaction"
+}
+
+var once = {
+    "pregunta11": "Nombra la banda sonora de la película más vendida de todos los tiempos",
+    "respuesta41": "I have Nothing",
+    "respuesta42": "My Love is Your Love",
+    "respuesta43": "Waiting to Exhale",
+    "respuestaC44": "The BodyGuard"
+}
+var deuce = {
+    "pregunta12": "¿Cual es el Primer Music Video Pop que salio?",
+    "respuestaC45": "Bohemian Rhapsody",
+    "respuesta46": "In My Feelings",
+    "respuesta47": "PlutoMania",
+    "respuesta48": "Man of the Woods"
+}
+var trec ={
+    "pregunta13":"¿A que edad murieron Janis Joplin, Jimi Hendrix y Kurt Cobain?",
+    "respuesta49": "21",
+    "respuesta50": "30",
+    "respuestaC51": "27",
+    "respuesta52": "25"
+}
+var foten ={
+    "pregunta14":"¿Cuantos Grammys gano Elvis Presley?",
+    "respuesta53": "4",
+    "respuestaC54": "3",
+    "respuesta55": "6",
+    "respuesta56": "8"
+}
+var fite ={
+    "pregunta15":"¿Cual es el mayor de los Hermanos Jackson?",
+    "respuestaC57": "Jackie Jackson",
+    "respuesta58": "Michael Jackson",
+    "respuesta59": "Randy Jackson",
+    "respuesta60": "Jermaine Jackson"
+}
+var rick ={
+    "pregunta16":"¿Cuantos Años tenia la persona mas joven en haber ganado un Grammy?",
+    "respuesta61": "5",
+    "respuesta62": "7",
+    "respuestaC63": "8",
+    "respuesta64": "9"
+}
+var morty ={
+    "pregunta17":"¿Quien es el artista con mas nominaiciones Grammy en 1 noche?",
+    "respuestaC65": "Michael Jackson",
+    "respuesta66": "Kendrick Lamar",
+    "respuesta67": "Kanye West",
+    "respuesta68": "Santana"
+}
+var ochos ={
+    "pregunta18":"¿Quien es el artista con mas nominaciones Grammys en la historia?",
+    "respuesta69": "Beyonce",
+    "respuestaC70": "Jay-Z",
+    "respuesta71": "Stevie Wonder",
+    "respuesta72": "John Williams"
+}
+var pac ={
+    "pregunta19":"¿Cuantos Grammys Gano Michael Jackson en 1 noche?",
+    "respuesta73": "3",
+    "respuesta74": "12",
+    "respuesta75": "19",
+    "respuestaC76": "8"
+}
+var veinte ={
+    "pregunta20":"¿Quien es conciderado el rapero mas con mas influencia en la Historia?",
+    "respuesta77": "Jay-Z",
+    "respuesta78": "Notorious B.I.G",
+    "respuestaC79": "2Pac",
+    "respuesta80": "Eminem"
+}
+var preguntas = [uno.pregunta1, dos.pregunta2, tres.pregunta3, fo.pregunta4, cinco.pregunta5, six.pregunta6, siete.pregunta7, ocho.pregunta8, nueve.pregunta9, diez.pregunta10, once.pregunta11, deuce.pregunta12, trec.pregunta13, foten.pregunta14, fite.pregunta15, rick.pregunta16, morty.pregunta17, ochos.pregunta18, pac.pregunta19, veinte.pregunta20]
+
+var respuestas = [
+    [uno.respuestaC1, uno.respuesta2, uno.respuesta3, uno.respuesta4],
+    [dos.respuesta5, dos.respuesta6, dos.respuesta7, dos.respuestaC8],
+    [tres.repuesta9, tres.respuestaC10, tres.respuesta11, tres.respuesta12],
+    [fo.respuesta13, fo.respuesta14, fo.respuestaC15, fo.respuesta16],
+    [cinco.respuesta17, cinco.respuestaC18, cinco.respuesta19, cinco.respuesta20],
+    [six.respuestaC21, six.respuesta22, six.respuesta23, six.respuesta24],
+    [siete.respuestaC25, siete.respuesta26, siete.respuesta27, siete.respuesta28],
+    [ocho.respuesta29, ocho.respuestaC30, ocho.respuesta31, ocho.respuesta32],
+    [nueve.respuesta33, nueve.respuesta34, nueve.respuestaC35, nueve.respuesta36],
+    [diez.respuesta37, diez.respuesta38, diez.respuestaC39, diez.respuesta40],
+    [once.respuesta41, once.respuesta42, once.respuesta43, once.respuestaC44],
+    [deuce.respuestaC45, deuce.respuesta46, deuce.respuesta47, deuce.respuesta48],
+    [trec.respuesta49, trec.respuesta50, trec.respuestaC51, trec.respuesta52],
+    [foten.respuesta53, foten.respuestaC54, foten.respuesta55, foten.respuesta56],
+    [fite.respuestaC57, fite.respuesta58, fite.respuesta59, fite.respuesta60],
+    [rick.respuesta61, rick.respuesta62, rick.respuestaC63, rick.respuesta64],
+    [morty.respuestaC65, morty.respuesta66, morty.respuesta67, morty.respuesta68],
+    [ochos.respuesta69, ochos.respuestaC70, ochos.respuesta71, ochos.respuesta72],
+    [pac.respuesta73, pac.respuesta74, pac.respuesta75, pac.respuestaC76],
+    [veinte.respuesta77, veinte.respuesta78, veinte.respuestaC79, veinte.respuesta80]
+]
+
+var correctas = ["0", "3", "1", "2", "1", "0", "0", "1", "2", "3", "0", "1", "0", "2", "0", "1", "3", "2"]
+
+var count = 0;
+
+
+function beg(){
+    Rand = Math.floor(Math.random() * 19);
+    document.getElementById("p1").innerHTML = preguntas[Rand];
+    document.getElementById("r1").innerHTML = respuestas[Rand][0];
+    document.getElementById("r2").innerHTML = respuestas[Rand][1];
+    document.getElementById("r3").innerHTML = respuestas[Rand][2];
+    document.getElementById("r4").innerHTML = respuestas[Rand][3];
+    RandO = Math.floor(Math.random() * 19);
+    document.getElementById("p2").innerHTML = preguntas[RandO];
+    document.getElementById("r5").innerHTML = respuestas[RandO][0];
+    document.getElementById("r6").innerHTML = respuestas[RandO][1];
+    document.getElementById("r7").innerHTML = respuestas[RandO][2];
+    document.getElementById("r8").innerHTML = respuestas[RandO][3];
+    Randi = Math.floor(Math.random() * 19);
+    document.getElementById("p3").innerHTML = preguntas[Randi];
+    document.getElementById("r9").innerHTML = respuestas[Randi][0];
+    document.getElementById("r10").innerHTML = respuestas[Randi][1];
+    document.getElementById("r11").innerHTML = respuestas[Randi][2];
+    document.getElementById("r12").innerHTML = respuestas[Randi][3];
+    Randu = Math.floor(Math.random() * 19);
+    document.getElementById("p3").innerHTML = preguntas[Randu];
+    document.getElementById("r13").innerHTML = respuestas[Randu][0];
+    document.getElementById("r14").innerHTML = respuestas[Randu][1];
+    document.getElementById("r15").innerHTML = respuestas[Randu][2];
+    document.getElementById("r16").innerHTML = respuestas[Randu][3];
+    Randf = Math.floor(Math.random() * 19);
+    document.getElementById("p4").innerHTML = preguntas[Randf];
+    document.getElementById("r17").innerHTML = respuestas[Randf][0];
+    document.getElementById("r18").innerHTML = respuestas[Randf][1];
+    document.getElementById("r19").innerHTML = respuestas[Randf][2];
+    document.getElementById("r20").innerHTML = respuestas[Randf][3];
+    Rands = Math.floor(Math.random() * 19);
+    document.getElementById("p5").innerHTML = preguntas[Rands];
+    document.getElementById("r17").innerHTML = respuestas[Rands][0];
+    document.getElementById("r18").innerHTML = respuestas[Rands][1];
+    document.getElementById("r19").innerHTML = respuestas[Rands][2];
+    document.getElementById("r20").innerHTML = respuestas[Rands][3];
+}
+
+function GetSelected() {
+    var arr = new Array();
+    var userChecked = questionario.getElementsByTagName("INPUT");
+  
+      for (var i = 0; i < userChecked.length; i++) {
+        if (userChecked[i].checked) {
+          arr.push(userChecked[i].value);
+          }
+      }
+}
+
+function form(){
+    var x = document.getElementById("myForm").elements.length;
+}
+
+function next(){
+    Rand = Math.floor(Math.random() * 19);
+    document.getElementById("p1").innerHTML = preguntas[Rand];
+    document.getElementById("r1").innerHTML = respuestas[Rand][0];
+    document.getElementById("r2").innerHTML = respuestas[Rand][1];
+    document.getElementById("r3").innerHTML = respuestas[Rand][2];
+    document.getElementById("r4").innerHTML = respuestas[Rand][3];
+    count ++;
+}
+    if(count == 5){
+        document.getElementById('next').style.display = "none";
+        }
+        else{
+            document.getElementById('sig').style.display = 'block';
+        }
+
+
